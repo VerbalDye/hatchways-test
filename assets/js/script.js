09a23172ed6d19562817ee5bb5732d5d520fb094
Dustin Huntsberger <dhunts258@gmail.com>
2022-04-14 15:46:31 -0400

    added test list

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,16 +24,18 @@ const renderStudents = studentData => {
         let studentEl = document.createElement("article");
 
         studentEl.innerHTML = `
-        <img src=${studentData[i].pic}>
-        <div>
-            <h2>${studentData[i].firstName + " " + studentData[i].lastName}</h2>
-            <p>Email: ${studentData[i].email}</p>
-            <p>Company: ${studentData[i].company}</p>
-            <p>Skill: ${studentData[i].skill}</p>
-            <p>Average: ${getAverageGrades(studentData[i].grades)}%</p>
-            <div class="popout" id="${"popout-" + i}" data-state="closed">${getTestList(studentData[i].grades)}</div>
+        <div class="article-content">
+            <img src=${studentData[i].pic}>
+            <div class="article-text">
+                <h2>${studentData[i].firstName + " " + studentData[i].lastName}</h2>
+                <p>Email: ${studentData[i].email}</p>
+                <p>Company: ${studentData[i].company}</p>
+                <p>Skill: ${studentData[i].skill}</p>
+                <p>Average: ${getAverageGrades(studentData[i].grades)}%</p>
+                <div class="popout" id="popout-${i}" data-state="closed">${getTestList(studentData[i].grades)}</div>
+            </div>
         </div>
-        <button data-element="${i}">+</button>
+        <button data-element="${i}"><i class="bi bi-plus-lg"></i></button>
         `
         mainSectionEl.appendChild(studentEl);
     }
@@ -55,6 +57,23 @@ const getTestList = tests => {
     return testsOutput
 }
 
+const handleMainClick = event => {
+    if (event.target.matches("i")) {
+        var buttonEl = event.target.parentElement;
+        var popoutEl = document.querySelector("#popout-" + buttonEl.getAttribute("data-element"));
+        console.log(popoutEl);
+        if (popoutEl.getAttribute("data-state") == "closed") {
+            popoutEl.style.maxHeight = '225px';
+            buttonEl.innerHTML = '<i class="bi bi-dash-lg"></i>';
+            popoutEl.setAttribute("data-state", "open");
+        } else {
+            popoutEl.style.maxHeight = '0';
+            buttonEl.innerHTML = '<i class="bi bi-plus-lg"></i>'
+            popoutEl.setAttribute("data-state", "closed");
+        }
+    }
+}
+
 const filterResults = () => {
     const inputText = nameInputEl.value.toLowerCase();
     const searchResults = studentResults.filter(student => {
@@ -71,5 +90,5 @@ const filterResults = () => {
 
 getStudents();
 
-mainEl.addEventListener("click", )
-nameInputEl.addEventListener("input", filterResults)
+mainEl.addEventListener("click", handleMainClick);
+nameInputEl.addEventListener("input", filterResults);
